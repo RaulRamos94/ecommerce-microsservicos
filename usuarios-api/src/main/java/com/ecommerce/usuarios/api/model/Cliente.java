9cab832b8cfb8f4daf4f487522634af5b535ffc6
Alexandre <souzajr.alexandre@gmail.com>
2024-05-07 21:54:25 -0300

    Criação das classes de usuarios-api

---
+++ usuarios-api/src/main/java/com/ecommerce/usuarios/api/model/Cliente.java
@@ -0,0 +1,46 @@
+package com.ecommerce.usuarios.api.model;
+
+import java.time.LocalDate;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embedded;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity(name = "tb_clientes")
+public class Cliente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nome;
+
+    @Column(nullable = false, unique = true)
+    private String cpf;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String telefone;
+
+    @Column(nullable = false)
+    @JsonFormat(pattern = "dd/MM/yyyy")
+    private LocalDate dataNascimento;
+
+    @Embedded
+    private Endereco endereco;
+
+}
---
+++ usuarios-api/src/main/java/com/ecommerce/usuarios/api/model/Endereco.java
@@ -0,0 +1,22 @@
+package com.ecommerce.usuarios.api.model;
+
+import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Embeddable
+public class Endereco {
+
+    private String cep;
+    private String logradouro;
+    private String cidade;
+    private String uf;
+    private String bairro;
+    private String numero;
+    private String complemento;
+
+}
