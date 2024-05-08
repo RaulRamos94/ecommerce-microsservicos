29cdbf59fce845e038db0a1c3a6ce7be451402ca
RaulRamos94 <raulfernandes3@hotmail.com>
2024-05-07 21:33:17 -0300

    Create Tables Clientes e Fornecedores

---
+++ usuarios-api/src/main/java/com/ecommerce/usuarios/api/model/Endereco.java
@@ -0,0 +1,16 @@
+package com.ecommerce.usuarios.api.model;
+
+import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Embeddable
+public class Endereco {
+
+    private String cep;
+    
+}
---
+++ usuarios-api/src/main/resources/db/migration/V1__CREATE-TABLE-CLIENTES.sql
@@ -0,0 +1,17 @@
+CREATE TABLE tb_clientes (
+
+    id BIGINT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(255) NOT NULL,
+    cpf VARCHAR(255) NOT NULL UNIQUE,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    telefone VARCHAR(255) NOT NULL,
+    dataNascimento DATE NOT NULL,
+    cep VARCHAR(255),
+    logradouro VARCHAR(255),
+    cidade VARCHAR(255),
+    uf VARCHAR(255),
+    bairro VARCHAR(255),
+    numero VARCHAR(255),
+    complemento VARCHAR(255)
+    
+);
