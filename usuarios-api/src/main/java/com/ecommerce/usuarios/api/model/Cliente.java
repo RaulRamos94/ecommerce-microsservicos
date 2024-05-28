3abb48cb4ec1ab7aa2428f40267c518dd7b969bd
Alexandre <souzajr.alexandre@gmail.com>
2024-05-28 20:23:32 -0300

    Adicionando novos atributos de usuário

--- usuarios-api/src/main/java/com/ecommerce/usuarios/api/model/Cliente.java
+++ usuarios-api/src/main/java/com/ecommerce/usuarios/api/model/Cliente.java
@@ -2,6 +2,12 @@ package com.ecommerce.usuarios.api.model;
 
 import java.time.LocalDate;
 import java.time.Period;
+import java.util.Collection;
+import java.util.List;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import com.ecommerce.compras.client.usuario.ClienteDTO;
 import com.fasterxml.jackson.annotation.JsonFormat;
@@ -20,7 +26,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity(name = "tb_clientes")
-public class Cliente {
+public class Cliente implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -31,6 +37,9 @@ public class Cliente {
 
     @Column(nullable = false, unique = true)
     private String cpf;
+
+    @Column(nullable = false)
+    private String senha;
 
     @Column(nullable = false, unique = true)
     private String email;
@@ -44,6 +53,12 @@ public class Cliente {
 
     @Embedded
     private Endereco endereco;
+
+    @Column(nullable = false)
+    private boolean administrador;
+
+    @Column(nullable = false)
+    private boolean usuarioExterno;
 
     public ClienteDTO converterParaDTO() {
         ClienteDTO dto = new ClienteDTO();
@@ -67,6 +82,49 @@ public class Cliente {
         dto.setComplemento(endereco.getComplemento());
 
         return dto;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        if (this.administrador) {
+            return List.of(new SimpleGrantedAuthority("ROLE_ADMIN"));
+        }
+
+        if (this.usuarioExterno) {
+            return List.of(new SimpleGrantedAuthority("ROLE_USER"));
+        }
+
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return senha;
+    }
+
+    @Override
+    public String getUsername() {
+        return cpf;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
     }
 
 }
